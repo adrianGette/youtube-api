3384a769fbd563f97cf0d1d3cc643b7f71e08f96
Adrian Gette <adrian.gette@icloud.com>
2021-08-31 15:21:27 -0300

    Receive data from youtube api

--- src/App.js
+++ src/App.js
@@ -2,21 +2,35 @@ import React from 'react';
 
 import { Grid } from '@material-ui/core';
 
-import { SearchBar, VideoList, VideoDetail } from './components';
+import { SearchBar, VideoDetail } from './components';
 
 import youtube from './api/youtube';
 
 class App extends React.Component {
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: '[API KEY]',
+                q: searchTerm,
+            }
+        });
+
+        console.log(response.data.items);
+    }
+
     render() {
         return (
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs={12}>
-                            {/* SEARCH BAR */}
+                            <SearchBar onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8}>
-                            {/* VIDEO DETAILS */}
+                            <VideoDetail />
                         </Grid>
                         <Grid item xs={4}>
                             {/* VIDEO LIST */}
